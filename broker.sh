fe2a7036957beb39148ca6d87566c270b01da449
Mattias Eklöf <mattias.eklof@gmail.com>
2021-05-19 11:03:21 +0200

    Broker script did not start a broker

--- broker.sh
+++ broker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -it -p 8000:8000 -e "KAFKA_REST_PROXY_URL=http://localhost:8082"  landoop/kafka-topics-ui
+docker run --rm -p 2181:2181 -p 3030:3030 -p 8081-8083:8081-8083 -p 9581-9585:9581-9585 -p 9092:9092 -e ADV_HOST=127.0.0.1 landoop/fast-data-dev:latest
